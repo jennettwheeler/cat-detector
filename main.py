c98b420ba97f0d9b124d756287b6486b638722eb
James Jennett-Wheeler <james.jennett-wheeler@ultraleap.com>
2024-07-01 13:45:41 +0100

    Run Yolo on webcam

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
+import cv2
+
 from ultralytics import YOLO
 
-# Create a new YOLO model from scratch
-model = YOLO("yolov8n.yaml")
-
-# Load a pretrained YOLO model (recommended for training)
 model = YOLO("yolov8n.pt")
 
-# Train the model using the 'coco8.yaml' dataset for 3 epochs
-results = model.train(data="coco8.yaml", epochs=3)
+cap = cv2.VideoCapture(0)
 
-# Evaluate the model's performance on the validation set
-results = model.val()
+while cap.isOpened():
+    success, frame = cap.read()
+    if success:
+        results = model(frame)
+        annotated_frame = results[0].plot()
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
 
-# Perform object detection on an image using the model
-results = model("https://ultralytics.com/images/bus.jpg")
-
-# Export the model to ONNX format
-success = model.export(format="onnx")
+cap.release()
+cv2.destroyAllWindows()
